efb79892d3bb979137628e6da842b3c005072ece
wiji <wijigaming.yt@gmail.com>
2023-07-30 22:20:13 -0700

    File input stream challenge 2

--- main.cpp
+++ main.cpp
@@ -3,44 +3,56 @@
 #include <string>
 #include <fstream>
 
-double calculate_score(std::string &response, std::string &key);
-
 int main() {
+    std::string to_lower(std::string &source);
 
-    std::fstream file_stream {"../responses.txt"};
+    std::fstream file_stream {"../source.txt"};
     if(!file_stream) {
         std::cerr << "Cannot locate file!" << std::endl;
         return 1;
     }
 
-    std::string answer_key {};
-    std::getline(file_stream, answer_key);
+    std::cout << "Please enter a word you would like to search for: " << std::endl;
 
-    std::string *student {nullptr};
+    std::string word;
+    std::cin >> word;
+
+    int total_word_count {1};
+    int selected_word_count {0};
+
+    char current_char;
+    std::string current_word;
+    bool last_char_space {false};
 
     while(!file_stream.eof()) {
-        if(student == nullptr) {
-            student = new std::string;
-            std::getline(file_stream, *student);
+        file_stream.get(current_char);
+
+        if(current_char == ' ' || current_char == '\n') {
+            if(last_char_space) continue;
+
+            last_char_space = true;
+            total_word_count++;
+            if(to_lower(current_word) == to_lower(word)) selected_word_count++;
+            current_word = "";
             continue;
-        }
+        } else last_char_space = false;
 
-        std::string response {};
-        std::getline(file_stream, response);
-
-        std::cout << *student << ": " << calculate_score(response, answer_key) * 100 << "%" << std::endl;
-
-        student = nullptr;
+        current_word += current_char;
     }
 
-    delete student;
+    std::cout << "Scanning " << total_word_count << " words..." << std::endl;
+    std::cout << "The source file has " << selected_word_count << " instances of the word \"" << word << "\"" << std::endl;
+
     return 0;
 }
 
-double calculate_score(std::string &response, std::string &key) {
-    int score {};
+std::string to_lower(std::string &source) {
+    std::string final {};
 
-    for(int i {}; i < key.length(); i++) if(key[i] == response[i]) score++;
+    for(auto c : source) {
+        final += static_cast<char>(tolower(c));
+    }
 
-    return score / static_cast<double>(key.length());
+    return final;
 }
+
