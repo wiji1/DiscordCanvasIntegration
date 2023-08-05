6cc65b2749d93d8c86877621aacfd9918424bacf
wiji <wijigaming.yt@gmail.com>
2023-08-05 01:15:45 -0700

    Added OTL dependency

--- main.cpp
+++ main.cpp
@@ -1,70 +1,73 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <fstream>
 #include <stack>
 #include <dpp/dpp.h>
+#include "SQL/SQLManager.h"
 
 std::string get_token();
+std::string get_sql_connection();
 void make_request(dpp::cluster &bot, const std::string &token);
 
-
 const std::string BOT_TOKEN {get_token()};
+const SQLManager SQL_MANAGER {get_sql_connection()};
 
 int main() {
-    dpp::cluster bot(BOT_TOKEN);
-
-    bot.on_log(dpp::utility::cout_logger());
-
-    bot.on_ready([&](const dpp::ready_t & event) {
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("verify", "verification", bot.me.id));
-        }
-    });
-
-    bot.on_slashcommand([&bot](const dpp::slashcommand_t & event) {
-        if (event.command.get_command_name() == "verify") {
-            dpp::interaction_modal_response modal("verification-form", "We need the following information from you");
-            modal.add_component(
-                    dpp::component().
-                            set_label("Canvas Access Token").
-                            set_id("access-token").
-                            set_type(dpp::cot_text).
-                            set_placeholder("Don't know how? Visit https://youtube.com/kys").
-                            set_min_length(5).
-                            set_max_length(100).
-                            set_text_style(dpp::text_short)
-            );
-            modal.add_row();
-            modal.add_component(
-                    dpp::component().
-                            set_label("Type rammel").
-                            set_id("field_id2").
-                            set_type(dpp::cot_text).
-                            set_placeholder("gumf").
-                            set_min_length(1).
-                            set_max_length(2000).
-                            set_text_style(dpp::text_paragraph)
-            );
-            event.dialog(modal);
-        }
-    });
-
-    bot.on_form_submit([&](const dpp::form_submit_t & event) {
-        /* For this simple example we know the first element of the first row ([0][0]) is value type string.
-         * In the real world it may not be safe to make such assumptions!
-         */
-        std::string v = std::get<std::string>(event.components[0].components[0].value);
-        dpp::message m;
-
-
-        make_request(bot, v);
-
-
-        m.set_content("You entered: " + v).set_flags(dpp::m_ephemeral);
-        event.reply(m);
-    });
-
-    bot.start(dpp::st_wait);
+//    dpp::cluster bot(BOT_TOKEN);
+//
+//    bot.on_log(dpp::utility::cout_logger());
+//
+//    bot.on_ready([&](const dpp::ready_t & event) {
+//        if (dpp::run_once<struct register_bot_commands>()) {
+//            bot.global_command_create(dpp::slashcommand("verify", "verification", bot.me.id));
+//        }
+//    });
+//
+//    bot.on_slashcommand([&bot](const dpp::slashcommand_t & event) {
+//        if (event.command.get_command_name() == "verify") {
+//            dpp::interaction_modal_response modal("verification-form", "We need the following information from you");
+//            modal.add_component(
+//                    dpp::component().
+//                            set_label("Canvas Access Token").
+//                            set_id("access-token").
+//                            set_type(dpp::cot_text).
+//                            set_placeholder("Don't know how? Visit https://youtube.com/kys").
+//                            set_min_length(5).
+//                            set_max_length(100).
+//                            set_text_style(dpp::text_short)
+//            );
+//            modal.add_row();
+//            modal.add_component(
+//                    dpp::component().
+//                            set_label("Type rammel").
+//                            set_id("field_id2").
+//                            set_type(dpp::cot_text).
+//                            set_placeholder("gumf").
+//                            set_min_length(1).
+//                            set_max_length(2000).
+//                            set_text_style(dpp::text_paragraph)
+//            );
+//            event.dialog(modal);
+//        }
+//    });
+//
+//    bot.on_form_submit([&](const dpp::form_submit_t & event) {
+//        /* For this simple example we know the first element of the first row ([0][0]) is value type string.
+//         * In the real world it may not be safe to make such assumptions!
+//         */
+//        std::string v = std::get<std::string>(event.components[0].components[0].value);
+//        dpp::message m;
+//
+//
+//        make_request(bot, v);
+//
+//
+//        m.set_content("You entered: " + v).set_flags(dpp::m_ephemeral);
+//        event.reply(m);
+//    });
+//
+//    bot.start(dpp::st_wait);
     return 0;
 }
 
@@ -80,6 +83,22 @@ std::string get_token() {
     if (!data.contains("bot-token")) throw std::runtime_error {"Config file does not contain bot token"};
 
     std::string token {data["bot-token"]};
+    input.close();
+    return token;
+}
+
+std::string get_sql_connection() {
+    std::ifstream input {"../resources/config.json"};
+    if (!input)
+        throw std::runtime_error {"Cannot locate config file"};
+
+    nlohmann::json data;
+    input >> data;
+    std::cout << data << std::endl;
+
+    if (!data.contains("sql-connection-string")) throw std::runtime_error {"Config file does not contain connection!"};
+
+    std::string token {data["sql-connection-string"]};
     input.close();
     return token;
 }
